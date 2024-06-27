cd7294e3a0d0f2d396974b7277a122e251724493
Kandla Gifari Akbar <kandlagifari@gmail.com>
2024-06-27 13:41:54 +0700

    resolved custom rule

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,12 @@ resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr_block
   enable_dns_hostnames = true
   tags = {
-    name = "main"
+    name        = "main"
+    Environment = "dev"
   }
 }
 
+# tfsec:ignore:MYCUSTOMCHECK_TAGS
 resource "aws_flow_log" "vpc_flow_log" {
   iam_role_arn    = aws_iam_role.example.arn
   log_destination = aws_cloudwatch_log_group.example.arn
@@ -16,6 +18,9 @@ resource "aws_flow_log" "vpc_flow_log" {
 resource "aws_cloudwatch_log_group" "example" {
   name       = "example"
   kms_key_id = "mykmskey"
+  tags = {
+    Environment = "dev"
+  }
 }
 
 data "aws_iam_policy_document" "assume_role" {
@@ -31,6 +36,7 @@ data "aws_iam_policy_document" "assume_role" {
   }
 }
 
+# tfsec:ignore:MYCUSTOMCHECK_TAGS
 resource "aws_iam_role" "example" {
   name               = "example"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
